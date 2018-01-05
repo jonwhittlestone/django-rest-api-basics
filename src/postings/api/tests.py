89edccadc261ed2a174f9a7c77afd131ccee1de7
Jon <dev@howapped.com>
2018-01-05 06:06:43 +0000

    basic test to GET list

--- src/postings/api/tests.py
+++ src/postings/api/tests.py
@@ -1,6 +1,8 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
 from postings.models import BlogPost
 from django.contrib.auth import get_user_model
+from rest_framework.reverse import reverse as api_reverse
 User = get_user_model()
 
 class BlogPostAPITestCase(APITestCase):
@@ -20,3 +22,9 @@ class BlogPostAPITestCase(APITestCase):
     def test_single_post(self):
         post_count = BlogPost.objects.count()
         self.assertEqual(post_count, 1)
+
+    def test_get_list(self):
+        data = {}
+        url = api_reverse('api-postings:post-listcreate') 
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- src/postings/api/urls.py
+++ src/postings/api/urls.py
@@ -2,6 +2,6 @@ from .views import BlogPostRudView, BlogPostAPIView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', BlogPostAPIView.as_view(), name='post-create'),
+    url(r'^$', BlogPostAPIView.as_view(), name='post-listcreate'),
     url(r'^(?P<pk>\d+)$', BlogPostRudView.as_view(), name='post-rud'),
 ]
